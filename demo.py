42d4541b1efcf42ec121c90731317642fecb08eb
vaishnavij7770 <vaishuj7770@gmail.com>
2026-06-27 13:18:35 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app=Flask(__name__)   #demo.py
+
+
+@app.route("/")
+def home():
+    return "Hello Flask"
+
+@app.route("/about")
+def about():
+    return "aboutpage"
+
+@app.route("/gallery")
+def gallery():
+    return "gallery"
+
+if __name__=="__main__":
+    app.run(debug=True,port=1234)
